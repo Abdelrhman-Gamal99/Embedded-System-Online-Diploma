9c3659f4fe9d118d96759439a38be4d0939dd520
Abdelrhman Gamal <abdo.gamal.ashour96@gmail.com>
2022-07-28 02:11:52 +0200

    problrm1 solved

---
+++ unit 2_C Programming/Assignment 1/problem1.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: 28 Jul 2022
+ *      Author: Abdelrhman
+ */
+
+/*
+  Print "C programming" sentence 
+*/
+
+#include "stdio.h"
+int main()
+{
+	printf("C Programming");
+	return 0 ;
+}
